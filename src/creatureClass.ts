4d965d9986ab94e91e36ed24492da87c5d5ce5cf
almost-jay <cassandra.grundy@gmail.com>
2022-11-16 10:58:19 +1100

    Improved comments and save/load

--- src/creatureClass.ts
+++ src/creatureClass.ts
@@ -18,22 +18,26 @@ export class creature {
 	segments : Array<creatureJoint>;
 	head : creatureHead;
 	legs : Array<creatureJoint>;
-	interacting : creature | null;
+	interactingId : string;
 	state : string;
 	traits : { [key: string] : number };
 	health : number;
 	energy : number;
 
-	constructor(pos : vector2, length : number, maxDist : number, weights : number) {
+	constructor(pos : vector2, length : number, maxDist : number, weights : number, id : string) {
 		this.pos = pos;
 		this.length = length;
 		this.maxDist = maxDist;
 		this.weights = weights;
 		
-		this.id = this.generateId();
+		if (id.length != 4) {
+			this.id = this.generateId();
+		} else {
+			this.id = id; //if an ID is given, it sets it to that ID
+		}
 		creaturesReference[this.id] = this;
-
-		this.mainArrayId = creatures.length
+		this.interactingId = "";
+		this.mainArrayId = creatures.length;
 		
 		this.legs = [];
 		this.state = "idle";
@@ -55,24 +59,45 @@ export class creature {
 		this.health = this.traits.maxHealth;
 	}
 
-	generateId() : string {
+	importJsonProps(loaded : creature) {
+		this.pos = new vector2(loaded.pos.x,loaded.pos.y);
+		this.target = new vector2(loaded.pos.x,loaded.pos.y);
+		this.path = [];
+		for (let i = 0; i < loaded.path.length; i++) {
+			this.path.push(new vector2(loaded.path[i].x,loaded.path[i].y));
+		}
+		this.legs = [];
+		this.segments = this.initSegments(loaded.length,loaded.maxDist,loaded.weights);
+		for (let i = 0; i < loaded.length; i++) {
+			this.segments[i] = Object.assign(this.segments[i],loaded.segments[i]);
+			this.segments[i].importJsonProps(loaded.segments[i]);
+		}
+		this.initSegmentChildren();
+		this.head = this.segments[0] as creatureHead;
+	}
+
+	generateId() : string { //gets a random id - if it already exists, it tries again, recursively
 		let idResult : Array<string> = [];
 		for (let i = 0; i < 4; i ++) {
 			idResult.push(idCharas[Math.floor(Math.random() * idCharas.length)]);
 		}
-		return idResult.join("");
+		if (idResult.join("") in creaturesReference) {
+			return this.generateId();
+		} else {
+			return idResult.join("");
+		}
 	}
 
 	initSegments(length : number, maxDist : number, weights : number) : Array<creatureJoint> {
-		let baseWidth = 4;
-		let bodyGuide = [[4,baseWidth]]; //4 is head
+		let baseWidth = 4; //the "base" width of a joint
+		let bodyGuide = [[4,baseWidth]]; //creates an array and puts a placeholder representing the head in it
 		
 		let tailLength = Math.floor(length / 2);
-		let weightDist = Math.ceil((length - tailLength) / weights);
+		let weightDist = Math.ceil((length - tailLength) / weights); //the distance between each set of legs/distribution of legs (idk what "dist" is short for)
 
-		let weightPoints = [-1];
-		for (let i = 1; i < length; i++) {
-			if (i > length - tailLength && weightPoints.length - 1 == weights) {
+		let weightPoints = [-1]; //makes an array with points of greatest thickness in it
+		for (let i = 1; i < length; i++) { //populates bodyGuide array with arrays containing a type (the number) and 0 (will later become the weight)
+			if (i > length - tailLength && weightPoints.length - 1 == weights) { //if the segment it's checking is not part of the tail and it's still got legs to add...
 				bodyGuide.push([1,0]); //tail
 			} else if ((i + 1) % weightDist === 0) {
 				bodyGuide.push([3,0]); //weightpoints
@@ -85,30 +110,29 @@ export class creature {
 			let distance = length;
 			for (let j = 0; j < weightPoints.length; j++) {
 				if (Math.abs(i - weightPoints[j]) < distance) {
-					distance = Math.abs(i - weightPoints[j]);
+					distance = Math.abs(i - weightPoints[j]); //checks for the closest leg joint
 				}
 			}
-			bodyGuide[i][1] = ((baseWidth * 2) - ((distance / length) * length)) + (baseWidth * 0.25);
+			bodyGuide[i][1] = ((baseWidth * 2) - ((distance / length) * length)) + (baseWidth * 0.25); //sets the weight of the joint it's checking to 
 		}
 
-		let colour = this.generateColours(3);
-		let segmentResult : Array<creatureJoint> = [];
-		let eyeColour = "#fafafa";
-
-		for (let i = 0; i < length; i++) {
+		let colour = this.generateColours(); //generates an array of colours
+		let segmentResult : Array<creatureJoint> = []; //initialises final list of segments to return
+		let eyeColour = "#fafafa"; //sets eye colour
+		for (let i = 0; i < length; i++) { //repeating for the desired length, it checks the corresponding bodyGuide joint
 			switch(bodyGuide[i][0]) {
-				case 1:
+				case 1: //if it's 1, it adds a new tail joint
 					segmentResult.push(new creatureTail(this.pos.add(new vector2(i * maxDist,i * maxDist)),i,colour[i],bodyGuide[i][1] * 0.8));
 					break;
-				case 2:
+				case 2: //if it's 2, it adds a new "generic" joint
 					segmentResult.push(new creatureJoint(this.pos.add(new vector2(i * maxDist,i * maxDist)),i,colour[i],bodyGuide[i][1]));
 					break;
-				case 3:
+				case 3: //if it's 3, it adds a new leg joint
 					let newJoint = new creatureLegJoint(this.pos.add(new vector2(i * maxDist,i * maxDist)),i,colour[i],bodyGuide[i][1] * 1.3,bodyGuide[i][1] * 0.8);
 					segmentResult.push(newJoint);
 					this.legs.push(newJoint);
 					break;
-				case 4:
+				case 4: //if it's 4, it adds a head!
 					segmentResult.push(new creatureHead(this.pos.add(new vector2(i * maxDist,i * maxDist)),0,colour[i],bodyGuide[i][1] * 1.4,eyeColour,bodyGuide[i][1] / 2));
 					break;
 			}
@@ -116,13 +140,13 @@ export class creature {
 		return segmentResult;
 	}
 
-	initSegmentChildren() : void {
+	initSegmentChildren() : void { //sets all segment joint children to their corresponding joint
 		for (let i = 0; i < this.segments.length - 1; i++) {
 			this.segments[i].childJoint = this.segments[i + 1];
 		}
 	}
 
-	generatePath(startPos : vector2) : Array<vector2> {
+	generatePath(startPos : vector2) : Array<vector2> { //generates a 
 		let pathResult : Array<vector2> = [];
 
 		if (startPos.x < 128) {
@@ -157,7 +181,7 @@ export class creature {
 		return pathResult;
 	}
 
-	generateColours(n : number) : any {
+	generateColours() : any {
 		let colourInd1 = Math.round(((Math.random() + preColours.length * 0.25) * (preColours.length - preColours.length * 0.25)));
 		let colourInd2 = Math.round(colourInd1 + ((Math.random() + preColours.length * 0.25) * (preColours.length - preColours.length * 0.25)));
 		
@@ -192,21 +216,21 @@ export class creature {
 					this.path.shift();
 					this.target = this.path[0];
 				}
-				if (this.interacting) {
-					if (this.interacting.state != "dead") {
-						this.interacting = null;
+				if (this.interactingId != "") {
+					if (creaturesReference[this.interactingId].state != "dead") {
+						this.interactingId = "";
 					}
 				}
 				break;
 			case "bitePrepare":
-				if (this.interacting) {
-					if (this.pos.distance(this.interacting.pos) < this.traits.lungeDistance) {
+				if (this.interactingId != "") {
+					if (this.pos.distance(creaturesReference[this.interactingId].pos) < this.traits.lungeDistance) {
 						let retreat = Math.round(Math.random());
 						if (retreat) {
 							this.target = this.segments[1].pos;
 							for (let i = 0; i < this.segments.length; i++) {
-								this.segments[i].pos.x += ((this.segments[i].pos.x - this.interacting.segments[1].pos.x) / 200) * (this.head.speed * 0.2);
-								this.segments[i].pos.y += ((this.segments[i].pos.y - this.interacting.segments[1].pos.y) / 200) * (this.head.speed * 0.2);
+								this.segments[i].pos.x += ((this.segments[i].pos.x - creaturesReference[this.interactingId].segments[1].pos.x) / 200) * (this.head.speed * 0.2);
+								this.segments[i].pos.y += ((this.segments[i].pos.y - creaturesReference[this.interactingId].segments[1].pos.y) / 200) * (this.head.speed * 0.2);
 							}
 						} else if (this.pos.distance(this.target) < this.maxDist){
 							this.target = new vector2(this.head.pos.x + ((Math.random() * this.maxDist * 2) + this.maxDist * -2),this.head.pos.y + ((Math.random() * this.maxDist * 2) + this.maxDist * -2));
@@ -216,7 +240,7 @@ export class creature {
 						if (lunge < 0.01) {
 							this.head.speed *= 1.6;
 							
-							this.target = this.interacting.segments[2].pos;
+							this.target = creaturesReference[this.interactingId].segments[2].pos;
 							this.target.x += (Math.random() - 0.5) * this.maxDist;
 							this.target.y += (Math.random() - 0.5) * this.maxDist;
 							
@@ -224,7 +248,7 @@ export class creature {
 						} else {
 							if (this.head.pos.distance(this.target) < this.maxDist) {
 								if (!this.path.length) {
-									this.path = this.generatePath(this.interacting.pos);
+									this.path = this.generatePath(creaturesReference[this.interactingId].pos);
 									this.target = this.path[0];
 								} else if (this.head.pos.distance(this.target) < this.maxDist) {
 									this.path.push(this.target);
@@ -237,8 +261,8 @@ export class creature {
 				}
 				break;
 			case "biteLunge":
-				if (this.head.pos.distance(this.target) < this.maxDist && this.interacting) {
-					this.interacting.takeDamage(this);
+				if (this.head.pos.distance(this.target) < this.maxDist && creaturesReference[this.interactingId]) {
+					creaturesReference[this.interactingId].takeDamage(this);
 					this.head.speed /= 2;
 					this.path = this.generatePath(this.pos);
 					this.target = this.path[0];
@@ -246,8 +270,8 @@ export class creature {
 				}
 				break;
 			case "threatened":
-				if (this.interacting) {
-					if (this.head.pos.distance(this.interacting.pos) < this.traits.lungeDistance / 2) {
+				if (this.interactingId != "") {
+					if (this.head.pos.distance(creaturesReference[this.interactingId].pos) < this.traits.lungeDistance / 2) {
 						this.target = this.segments[this.segments.length - 1].pos;
 					} else {		
 						if (Math.random() < 0.01) {
@@ -264,7 +288,7 @@ export class creature {
 
 	castSight() : void {
 		let canSee = [];
-		if (!this.interacting && this.state != "dead") {
+		if (this.interactingId == "" && this.state != "dead") {
 			for (let i = 0; i < creatures.length; i++) {
 				let distanceTo = creatures[i].pos.distance(this.pos);
 				if (distanceTo < this.traits.sightDistance && creatures[i].id != this.id) {
@@ -287,10 +311,10 @@ export class creature {
 	seeCreature(creature : creature) {
 		let sightAngle = Math.abs(this.head.angle - creature.pos.getAvgAngleDeg(this.head.pos));
 		if (sightAngle < 36) {
-			this.interacting = creature;
+			this.interactingId = creature.id;
 			this.initAttack();
 			creature.state = "threatened";
-			creature.interacting = this;
+			creature.interactingId = this.id;
 		}
 	}
 
@@ -300,7 +324,7 @@ export class creature {
 	}
 
 	takeDamage(attacker : creature) : void {
-		attacker.interacting = null;
+		attacker.interactingId = "";
 		let damage = Math.floor((Math.random() * 18) + 8);
 		if (this.health - damage < 1) {
 			this.initCorpse(attacker);
@@ -321,8 +345,8 @@ export class creature {
 	initCorpse(killer : creature) : void {
 		this.state = "dead";
 		this.health = 0;
-		this.interacting = killer;
-		killer.interacting = this;
+		this.interactingId = killer.id;
+		killer.interactingId = this.id;
 		this.head.eyeType = "cross";
 		this.path = [];
 		creatures.splice(this.mainArrayId,1);
@@ -339,27 +363,12 @@ export class creature {
 	}
 
 	renderCreature() : void {
-		// for (let i = 0; i < this.path.length; i++) {
-		// 	ctx.fillStyle = "green";
-		// 	ctx.beginPath();
-		// 	ctx.arc(this.path[i].x,this.path[i].y,4,0,2 * Math.PI);
-		// 	ctx.fill();
-		// }
-
 		ctx.lineCap = "round";
 		for (let i = this.segments.length - 2; i >= 0; i -= 1) {
 			if (this.segments[i].pos.x > activeArea[0].x && this.segments[i].pos.y > activeArea[0].y && this.segments[i].pos.x < activeArea[1].x && this.segments[i].pos.y < activeArea[1].y) {
 				this.segments[i].renderSegment();
 			}
 		}
-		// ctx.font = "bold 12px Kanit";
-		// ctx.fillStyle = (this.head.eyeColour);
-		// ctx.fillText(this.id,this.head.pos.x, this.head.pos.y - 18);
-		// ctx.fillStyle = ("#AEAEAE");
-		// ctx.fillText(this.state,this.head.pos.x,this.head.pos.y - 27);
-		// if (this.interacting) {
-		// 	ctx.fillText(this.interacting.id,this.head.pos.x,this.head.pos.y - 36);
-		// }
 	}
 	
 	updateCreature() : void {
@@ -367,14 +376,15 @@ export class creature {
 		this.stateMachine();
 		this.renderCreature();
 		for (let i = 0; i < this.segments.length - 1; i++) {
-			this.segments[i].move(this.maxDist,this.target,false);
+			let angleParentPos = this.segments[Math.max(i - Math.ceil(this.length / this.weights),0)].pos;
+			this.segments[i].move(this.maxDist,this.target,false,angleParentPos);
 		}
 		this.pos = this.head.pos.getAvgPos(this.segments[this.segments.length - 1].pos);
 		this.head.moveHead(this.target);
 	}
 
 	renderCorpse() : void {
-		this.target = this.interacting!.head.pos;
+		this.target = creaturesReference[this.interactingId].head.pos;
 
 		let centreIndex = Math.floor(this.segments.length / 2) - 2;
 		this.segments[centreIndex].pos = this.target;
@@ -391,7 +401,8 @@ export class creature {
 			this.segments[i].pos = this.segments[i].pos.add(offsetBack.multiply(1 / (i * flexibility)));
 		}
 		for (let i = 1; i < this.segments.length - 1; i++) {
-			this.segments[i].move(this.maxDist,this.target,true);
+			let angleParentPos = this.segments[Math.max(i - Math.ceil(this.length / this.weights),0)].pos;
+			this.segments[i].move(this.maxDist,this.target,true,angleParentPos);
 		}
 		
 		this.pos = this.head.pos.getAvgPos(this.segments[this.segments.length - 1].pos);
